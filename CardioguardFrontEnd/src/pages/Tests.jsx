67b2e66fc333bc124d4868d6e562de26254c067a
drpnilupul <150885432+pradeep512@users.noreply.github.com>
2024-09-17 17:18:21 +0530

    Removed Unnecessary patientHomeComponent

--- CardioguardFrontEnd/src/pages/Tests.jsx
+++ CardioguardFrontEnd/src/pages/Tests.jsx
@@ -11,63 +11,16 @@ import PatientExaminationDataById from "../components/GetDoctorExaminationsById"
 import PatientDataById from "../components/GetPatientById";
 import PatientSymptomDataById from "../components/GetPatientSymptomsById";
 import DoctorPatients from "../components/GetRegisteredPatientsForDoctor";
-import PatientSymptomDataByBothId from "../components/GetSymptomsDataByBothIDs";
-import MachineLearningData from "../components/MachineLerningDataForm";
-import NavBar from "../components/NavBar";
+
 import PatientClinicalDataById from "../components/PatientClinicalDataById";
-import PatientClinicalDataSubmission from "../components/PatientClinicalDataSubmission ";
-import PatientProfileHeader from "../components/PatientHomePageComponents/PatientProfileHeader";
-import PatientGeneralInfo from "../components/PatientHomePageComponents/PatientGeneralInfo";
-import PatientContactInfo from "../components/PatientHomePageComponents/PatientContactInfo";
-import PatientSymptomsSubmit from "../components/PatientSymptomsSubmit";
-import PredictionButton from "../components/PredictionButton";
+
 import RegisterPatientToDoctor from "../components/RegisterPatientsForDoctor";
-import SearchDoctors from "../components/SearchDoctors";
-import SearchPatients from "../components/SearchPatients";
-import SendDeleteButton from "../components/SendDeleteButton";
-import UpdateDoctor from "../components/UpdateDoctor ";
-import UpdateSymptoms from "../components/UpdateSymptoms";
-import UpdateSymptomsByBothIds from "../components/UpdateSymptomsByBothIds";
-import PatientHome from "./Patient/PatientHomePage";
+
 // import AdminHome from "./Admin/AdminPage";
 
 export default function Tests() {
   return (
     <div>
-      {/* <NavBar />
-      <MachineLearningData />
-      <DeleteDoctor />
-      <FetchAllDoctors />
-      <DeletePatient />
-      <DeleteSymptoms />
-      <DoctorExaminationDataSubmission />
-      <FetchAllPatients />
-      <PatientClinicalDataById />
-      <DoctorDataById />
-      <PatientExaminationDataById />
-      <PatientDataById />
-      <PatientSymptomDataById />
-      <DoctorPatients />
-      <PatientSymptomDataByBothId /> {/*  Something wrong here */}
-      {/* <PatientClinicalDataSubmission />
-      <PatientSymptomsSubmit />
-      <PredictionButton />
-      <RegisterPatientToDoctor />
-      <SearchDoctors />
-      <SearchPatients /> */}
-      {/* <SendDeleteButton />  */}
-      {/* Don't know what is this button use for */}
-      {/* <UpdateDoctor />
-      <UpdateSymptoms /> */}
-      {/* <UpdateSymptomsByBothIds />  */}
-      {/*same as the UpdateSymptoms for now */}
-
-      {/* <PatientContactInfo/>
-      <PatientGeneralInfo/>
-      <PatientProfileHeader/>  */}
-
-      {/* <AdminHome /> */}
-      {/* <PatientHome/> */}
       <DeleteDoctor />
       <FetchAllDoctors />
       <DeletePatient />
